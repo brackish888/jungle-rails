834b8cc4fa610a835531cea572b0ced6cca536ae
vagrant <bbrackish@protonmail.com>
2021-04-29 04:00:50 +0000

    add an about page and appropriate routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
     post   :remove_item
   end
 
+  resources :about, only: [:index, :show]
+
   resources :orders, only: [:create, :show]
 
   namespace :admin do
